f0062082367742c00ed952c5843eac24e1a1f6aa
Andrii Sukhenko <andrii.suhenko@gmail.com>
2024-01-28 16:39:14 +0200

    add env

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': str(os.getenv("DB_ENGINE")),
+        'NAME': str(os.getenv("DB_NAME")),
+        'USER': str(os.getenv("DB_USER")),
+        'PASSWORD': str(os.getenv("DB_PASSWORD")),
+        'HOST': str(os.getenv("DB_HOST")),
+        'PORT': str(os.getenv("DB_PORT")),
     }
 }
 
