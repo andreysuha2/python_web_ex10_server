1de677148f9937361d9a1c2ab590e52941be29e1
Andrii Sukhenko <andreysuha2@gmail.com>
2024-01-29 18:46:24 +0200

    add main page

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpRequest
+
 
 # Create your views here.
+class MainPageView(View):
+    template_name = 'quotes/index.html'
+
+    def get(self, request: HttpRequest):
+        return render(request, self.template_name)
